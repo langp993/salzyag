bdd85fd38b68bf21fb33d009e35b078db2fae081
langp993 <patrickphillange@gmail.com>
2022-07-01 11:51:32 +0200

    commit trying to get boxes below gesamt preis to center and and go left at md

--- src/Pages/Step4.js
+++ src/Pages/Step4.js
@@ -4,24 +4,28 @@ import { DownloadIcon, ChevronRightIcon } from "@heroicons/react/outline";
 
 export default function Step4() {
   return (
-    <div className="px-4 mb-6 max-w-md mx-auto">
+    <div className="px-[6%] mb-6 bg-pink-100 mx-auto">
       <h3 className="font-bold">Ihr persönliches Angebot:</h3>
       <p className="mb-4">
         Wir haben auf Ihre Email Adresse folgende Dokumente zugesendet.
       </p>
 
-      <button className="flex w-full px-4 py-3 mx-auto mb-6 bg-white rounded-lg shadow-md">
+      <div class="p-2 bg-yellow-500 w-[100%] -sm:h-[250px] sm:h-[100px] h-26 grid grid-cols-1 sm:grid-cols-3 gap-4 content-start">
+
+      <button className="flex w-md px-4 py-3 h-[50px] mx-auto mb-6 bg-white rounded-lg shadow-md">
         <DownloadIcon className="w-6 h-6 mr-4 text-black" />
         Rahmenvertrag ansehen
       </button>
-      <button className="flex w-full px-4 py-3 mx-auto mb-6 bg-white rounded-lg shadow-md">
+      <button className="flex w-md px-4 py-3 mx-auto mb-6 bg-white rounded-lg shadow-md">
         <DownloadIcon className="w-6 h-6 mr-4 text-black" />
         Allgemeine Vertragsunterlagen
       </button>
-      <button className="flex w-full px-4 py-3 mx-auto mb-6 bg-white rounded-lg shadow-md">
+      <button className="flex w-md px-4 py-3 mx-auto mb-6 bg-white rounded-lg shadow-md">
         <DownloadIcon className="w-6 h-6 mr-4 text-black" />
         Beitrittserklärung
       </button>
+
+      </div>
 
       {/* <p className="mt-6">
         Wir schätzen Ihre monatliche Strom Ersparnis auf etwa{" "}
@@ -66,30 +70,46 @@ export default function Step4() {
         <div className="font-bold">¢ 28,0408 / 33,6490</div>
       </div>
     
+      <div className="bg-red-300 mx-auto h-[400px]">
 
-      <label className="flex justify-between py-0 px-0">
+      <div className="bg-yellow-200 items-center justify-center max-w-md">
+      <label className="flex py-0 px-0">
         <input type="checkbox" className="w-4 h-4 rounded mt-1.5 mr-4" />
         Hiermit erkläre ich ausdrücklich, dass die Salzburg <br/>
         AG mit der von mir beauftragte Dienstleitung vor Ablauf <br/>
         der 14 tägigen Rücktrittsfrist gemäß § 11 FAGG beginnen soll. <br/>
       </label>
+      </div>
 
+      <div className="bg-red-500 p-4">
+      <div className="bg-green-500 mx-auto">
       <Link
         to="/Step5"
-        className="flex justify-between px-4 py-3 mt-4 text-white rounded-lg bg-primary">
+        className="flex justify-between px-4 py-3 mt-4 text-white rounded-lg bg-primary max-w-md">
         Bestellung/Angebot kostenpflichtig abschicken
         <ChevronRightIcon className="w-6 h-6" />
       </Link>
+      </div>
+      </div>
 
+      <div className="bg-green-500 p-4">
+      <div className="bg-white mx-auto">
       <Link
         to="/erinnerung"
-        className="block px-4 py-3 mt-4 text-center rounded-lg shadow-lg">
+        className="block px-4 py-3 mt-4 text-center rounded-lg shadow-lg max-w-md mx-aut ">
         Erinnern Sie mich in einer Woche
       </Link>
+      </div>
+      </div>
 
+      <div className="bg-purple-500">
       <Link to="/keininteresse" className="block px-4 py-3 mt-4 text-center">
         Kein Interesse
       </Link>
+      </div>
+
+      </div>
+
     </div>
   );
 }
